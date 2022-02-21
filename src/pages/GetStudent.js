8d063c1265cfbc92104094c8494131c1d90e9e51
“anildalar” <dollorinfotech@gmail.com>
2022-02-21 09:55:53 +0530

    asd

---
+++ src/pages/GetStudent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Layout from '../components/Layout'
+
+export default function GetStudent() {
+    //1. State/Hooks Variable
+
+    let param = useParams()
+
+    //2. Function defination
+
+    //3. REturn statement JSX
+    return (
+        <Layout>
+            <h1>GetStudent {param.stu_id} {param.id} </h1>
+        </Layout>
+        
+    )
+}
